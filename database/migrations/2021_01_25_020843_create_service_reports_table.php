f2f97c5775a949be31193be38299dc1dfbcb92cb
Paul <paul.jao95@gmail.com>
2021-01-25 10:45:49 +0800

    added service report migration tables

---
+++ database/migrations/2021_01_25_020843_create_service_reports_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServiceReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('service_reports', function (Blueprint $table) {
+            $table->id();
+            $table->integer('csr_no');
+            $table->date('date');
+            $table->string('customer_name');
+            $table->string('customer_email')->nullable();
+            $table->string('address');
+            $table->string('call_status')->nullable();
+            $table->unsignedInteger('engineer_id');
+            $table->string('engineer_remark')->nullable();
+            $table->string('status_after_service')->nullable();
+            $table->dateTime('event');
+            $table->dateTime('service_start')->nullable();
+            $table->dateTime('service_end')->nullable();
+            $table->string('used_it_credit');
+            $table->string('signature_image')->nullable();
+            $table->date('signed_date');
+            $table->string('customer_comments')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('service_reports');
+    }
+}
