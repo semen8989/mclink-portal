bd99cbfb6be8402848fb952634238bcf49b2b991
Paul <paul.jao95@gmail.com>
2021-01-26 11:30:19 +0800

    update fields to reflect the changes in the migration file for service reports table

--- database/seeders/ServiceReportSeeder.php
+++ database/seeders/ServiceReportSeeder.php
@@ -18,12 +18,14 @@ class ServiceReportSeeder extends Seeder
     public function run()
     {
         DB::table('service_reports')->insert([
+            'id' => Str::uuid(),
             'csr_no' => Carbon::now()->format('Ymd'),
             'date' => Carbon::now()->format('Y-m-d'),
             'customer_name' => Str::random(10),
             'address' => Str::random(20),
             'engineer_id' => User::find(1)->id,
-            'event' => Carbon::now()->format('Y-m-d H:i:s'),
+            'current_user_id' => User::find(1)->id,
+            'service_rendered' => Str::random(100),
             'used_it_credit' => Str::random(10),
             'created_at' =>  Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' =>  Carbon::now()->format('Y-m-d H:i:s'),
