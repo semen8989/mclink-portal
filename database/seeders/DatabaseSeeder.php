55fe32b51f74af82c15a6d86b9b53c7c700c97ef
Paul <paul.jao95@gmail.com>
2021-01-26 11:31:06 +0800

    removed services table seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ServiceReportSeeder::class,
-            ServiceSeeder::class,
         ]);
     }
 }
