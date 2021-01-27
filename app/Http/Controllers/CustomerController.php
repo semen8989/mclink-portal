ffb293b085b5ddae3069f093d68eddbdfd6a266a
Paul <paul.jao95@gmail.com>
2021-01-27 17:09:37 +0800

    added method for typeahead routes

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Fetch the list of active customers.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function get(Request $request)
+    {            
+        $customers = Customer::select('id','name','email','address')
+            ->withTrashed(false)
+            ->where([['name', 'like', '%'.$request->get('term').'%']])
+            ->orderBy('name', 'asc')
+            ->paginate(10);
+
+        return response()->json($customers);
+    }
+}
