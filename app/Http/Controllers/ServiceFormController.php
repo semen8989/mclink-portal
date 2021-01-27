a50bcee431fff6215b24dffe26e93d9b20d93da3
Paul <paul.jao95@gmail.com>
2021-01-27 17:10:54 +0800

    added create and store method for the new routes

--- app/Http/Controllers/ServiceFormController.php
+++ app/Http/Controllers/ServiceFormController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class ServiceFormController extends Controller
 {
     /**
-     * Show the profile for a given user.
+     * Show list of service forms.
      *
      * @return \Illuminate\View\View
      */
@@ -15,5 +15,33 @@ class ServiceFormController extends Controller
     {
         return view('service_form.index', [
         ]);
+    }
+
+    /**
+     * Show the form for creating a new service form.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('service_form.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        switch ($request->input('action')) {
+            case 'save':
+                // Save model
+                break;
+    
+            case 'preview':
+                // Preview model
+                break;
+    
+            case 'advanced_edit':
+                // Redirect to advanced edit
+                break;
+        }
     }
 }
