948603f42bd158bbcd1372fac1ebe4c955716f34
Paul <paul.jao95@gmail.com>
2021-01-28 16:50:28 +0800

    regrouped routes and added names

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ServiceFormController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,13 +21,22 @@ Route::get('/', function () {
 })->middleware('auth');
 
 Route::middleware(['auth'])->group(function () {
-    // Service Form Routes
-    Route::get('/service-forms', [ServiceFormController::class, 'index']);
-    Route::get('/service-forms/create', [ServiceFormController::class, 'create']);
-    Route::post('/service-forms', [ServiceFormController::class, 'store']);
 
-    // Customer Routes
-    Route::get('/get/customers/typeahead', [CustomerController::class, 'get']);
+    // Service Form Routes
+    Route::prefix('service-forms')->group(function () {
+        Route::get('/', [ServiceFormController::class, 'index'])->name('service.form.index');
+        Route::get('/create', [ServiceFormController::class, 'create'])->name('service.form.create');
+        Route::post('/', [ServiceFormController::class, 'store'])->name('service.form.store');
+    });
+
+    // Typeahead Routes
+    Route::prefix('get')->group(function () {
+        // Customer Route
+        Route::get('/customers/typeahead', [CustomerController::class, 'get'])->name('get.customers');
+
+        // User Route
+        Route::get('/engineers/typeahead', [UserController::class, 'getEngineers'])->name('get.engineers');
+    }); 
 });
 
 Auth::routes(['register' => false]);
