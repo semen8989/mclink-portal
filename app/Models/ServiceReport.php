90e3a2fff87d7b28f785b89e0f28716ec5913997
Paul <paul.jao95@gmail.com>
2021-01-29 17:11:30 +0800

    added mutator for service_start and service_end fields

--- app/Models/ServiceReport.php
+++ app/Models/ServiceReport.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Customer;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,6 +12,35 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class ServiceReport extends Model
 {
     use HasFactory, SoftDeletes;
+        
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+    
+    /**
+     * Set the service start datetime format.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setServiceStartAttribute($value)
+    {
+        $this->attributes['service_start'] = Carbon::parse($value)->format('Y-m-d h:i:s');
+    }
+
+    /**
+     * Set the service end datetime format.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setServiceEndAttribute($value)
+    {
+        $this->attributes['service_end'] = Carbon::parse($value)->format('Y-m-d h:i:s');
+    }
 
     /**
      * Get the engineer user that is associated with the service report.
