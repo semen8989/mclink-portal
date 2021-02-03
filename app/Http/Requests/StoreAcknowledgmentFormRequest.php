67e6e4069e0a0b5f02e529aab401d93fa7b7d509
Paul <paul.jao95@gmail.com>
2021-02-04 05:44:29 +0800

    added the custom image validator in form request

--- app/Http/Requests/StoreAcknowledgmentFormRequest.php
+++ app/Http/Requests/StoreAcknowledgmentFormRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\Imageable;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreAcknowledgmentFormRequest extends FormRequest
@@ -26,7 +27,7 @@ class StoreAcknowledgmentFormRequest extends FormRequest
         return [
             'isAcknowledged' => 'bail|required|accepted',
             'signedCust' => 'bail|required',
-            'signatureDataUrl' => 'bail|required|image',
+            'signatureDataUrl' => ['bail', 'required', new Imageable],
         ];
     }
 }
