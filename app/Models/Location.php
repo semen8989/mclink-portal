8aa0c667e19efe139d8773a9a43dd2c754db3a32
Kassandra Lim <sandralim298@gmail.com>
2021-02-10 16:50:04 +0800

    removed 'table' and 'primaryKey' propertyon model

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Location extends Model
 {
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
     
     protected $fillable = [
         'company_id',
