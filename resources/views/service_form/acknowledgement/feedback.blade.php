0fc582649d540e2bceb72ec67308744ddc919df3
Paul <paul.jao95@gmail.com>
2021-02-17 05:10:22 +0800

    added feedback page for acknowledgement form

---
+++ resources/views/service_form/acknowledgement/feedback.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.guest.master')
+
+@section('content')
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="clearfix text-center">            
+                <h3 class="mb-4">An email has been sent to your email containing your copy of the service report</h3>
+                <p class="text-muted">
+                    Thank you for doing business with us. Can you take a couple of minutes to leave a feedback about your experience with us? 
+                    Just go to this <a href="">page</a>. Thanks for your help!
+                </p>
+            </div>
+        </div>
+    </div>
+@stop
