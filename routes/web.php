b50703fae8bcec2e7292402cd33ee127f3a960be
Paul <paul.jao95@gmail.com>
2021-02-17 05:03:32 +0800

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,14 @@ Route::middleware(['auth'])->group(function () {
         return view('dashboard');
     })->name('dashboard');
 
-    // Service Form Routes
+    // Service Report Routes
     Route::prefix('service-forms')->group(function () {
         Route::get('/', [ServiceFormController::class, 'index'])->name('service.form.index');
         Route::get('/create', [ServiceFormController::class, 'create'])->name('service.form.create');
         Route::post('/', [ServiceFormController::class, 'store'])->name('service.form.store');
         Route::get('/{serviceReport:csr_no}', [ServiceFormController::class, 'show'])->name('service.form.show');
         Route::get('/{serviceReport:csr_no}/edit', [ServiceFormController::class, 'edit'])->name('service.form.edit');
+        Route::put('/{serviceReport:csr_no}', [ServiceFormController::class, 'update'])->name('service.form.update');
         Route::delete('/{serviceReport:csr_no}', [ServiceFormController::class, 'destroy'])->name('service.form.destroy');
 
         Route::get('/{serviceReport:csr_no}/download', [ServiceFormController::class, 'download'])->name('service.form.download');
@@ -47,10 +48,11 @@ Route::middleware(['auth'])->group(function () {
 
 // Guest Routes
 
-// Service Form Routes
+// Acknowledgement Routes
 Route::prefix('service-form/acknowledgement')->group(function () {
     Route::get('/{serviceReport}/create', [AcknowledgementFormController::class, 'create'])->name('service.form.acknowledgment.create');
     Route::post('/{serviceReport}', [AcknowledgementFormController::class, 'store'])->name('service.form.acknowledgment.store');
+    Route::get('/feedback', [AcknowledgementFormController::class, 'feedback'])->name('service.form.acknowledgment.feedback');
 });
 
 Auth::routes(['register' => false]);
