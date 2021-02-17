695d9f2a8ee962025dea08c0d0e6b11c05d874e9
Paul Jao <paul.jao95@gmail.com>
2021-02-17 09:28:47 +0800

    added the static feedback form url

--- resources/views/service_form/acknowledgement/feedback.blade.php
+++ resources/views/service_form/acknowledgement/feedback.blade.php
@@ -7,7 +7,8 @@
                 <h3 class="mb-4">An email has been sent to your email containing your copy of the service report</h3>
                 <p class="text-muted">
                     Thank you for doing business with us. Can you take a couple of minutes to leave a feedback about your experience with us? 
-                    Just go to this <a href="">page</a>. Thanks for your help!
+                    Just go to this <a href="https://www.jotform.com/edit/4704343844176563397?utm_source=emailfooter&utm_medium=email&utm_term=83401151973453&utm_content=edit_submissions&utm_campaign=notification_email_footer_submission_links&email_type=notification" 
+                    target="_blank">page</a>. Thanks for your help!
                 </p>
             </div>
         </div>
