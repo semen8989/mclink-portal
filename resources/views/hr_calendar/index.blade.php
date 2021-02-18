1b836c198fa19ff00019f0dcfdf50e4112c7a742
Kassandra Lim <sandralim298@gmail.com>
2021-02-18 15:54:28 +0800

    fc-events design

--- resources/views/hr_calendar/index.blade.php
+++ resources/views/hr_calendar/index.blade.php
@@ -3,14 +3,45 @@
 @section('content')
 <div class="card-header">Hr Calendar</div>
 <div class="card-body">
-    <div id="calendar">
-
+    <div class="row">
+        <div class="col-md-3">
+            <div id="external-events">
+                <div id="external-events-listing" class="fc-events-container">
+                    <div class="fc-event" data-color="#2D95BF" style="background-color: rgb(45, 149, 191); border-color: rgb(45, 149, 191);">Holidays</div>
+                    <div class="fc-event" data-color="#48CFAE" style="background-color: rgb(72, 207, 174); border-color: rgb(72, 207, 174);">Leave Request</div>
+                    <div class="fc-event" data-color="#50C1E9" style="background-color: rgb(80, 193, 233); border-color: rgb(80, 193, 233);">Travel Request</div>
+                    <div class="fc-event" data-color="#FB6E52" style="background-color: rgb(251, 110, 82); border-color: rgb(251, 110, 82);">Upcoming Birthday</div>
+                    <div class="fc-event" data-color="#ED5564" style="background-color: rgb(237, 85, 100); border-color: rgb(237, 85, 100);">Trainings</div>
+                    <div class="fc-event" data-color="#355C7D" style="background-color: rgb(53, 92, 125); border-color: rgb(53, 92, 125);">Events</div>
+                    <div class="fc-event" data-color="#547A8B" style="background-color: rgb(84, 122, 139); border-color: rgb(84, 122, 139);">Meetings</div>
+                    <div class="fc-event" data-color="#3EACAB" style="background-color: rgb(62, 172, 171); border-color: rgb(62, 172, 171);">Goals</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-9">
+            <div id="calendar"></div>
+        </div>
     </div>
 </div>
 @stop
 
 @push('stylesheets')
 <link rel="stylesheet" href="{{ asset('plugin/fullcalendar/main.min.css') }}" />
+<style>
+    #external-events{
+        width: 240px;
+        padding: 0 10px;
+        border: 1px solid #ccc;
+        background: #eee;
+        text-align: left;
+    }
+    
+    #external-events .fc-event {
+        margin: 10px 0;
+        padding: 2px !important;
+        cursor: pointer;
+    }
+</style>
 @endpush
 
 @push('scripts')
