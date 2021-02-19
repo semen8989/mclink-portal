2c3986fa2c659fd7a0a63d05dab70a2528fd3909
Kassandra Lim <sandralim298@gmail.com>
2021-02-19 11:43:07 +0800

    Add event by clicking on calendar using modal

--- resources/views/hr_calendar/index.blade.php
+++ resources/views/hr_calendar/index.blade.php
@@ -7,14 +7,25 @@
         <div class="col-md-3">
             <div id="external-events">
                 <div id="external-events-listing" class="fc-events-container">
-                    <div class="fc-event" data-color="#2D95BF" style="background-color: rgb(45, 149, 191); border-color: rgb(45, 149, 191);">Holidays</div>
-                    <div class="fc-event" data-color="#48CFAE" style="background-color: rgb(72, 207, 174); border-color: rgb(72, 207, 174);">Leave Request</div>
-                    <div class="fc-event" data-color="#50C1E9" style="background-color: rgb(80, 193, 233); border-color: rgb(80, 193, 233);">Travel Request</div>
-                    <div class="fc-event" data-color="#FB6E52" style="background-color: rgb(251, 110, 82); border-color: rgb(251, 110, 82);">Upcoming Birthday</div>
-                    <div class="fc-event" data-color="#ED5564" style="background-color: rgb(237, 85, 100); border-color: rgb(237, 85, 100);">Trainings</div>
-                    <div class="fc-event" data-color="#355C7D" style="background-color: rgb(53, 92, 125); border-color: rgb(53, 92, 125);">Events</div>
-                    <div class="fc-event" data-color="#547A8B" style="background-color: rgb(84, 122, 139); border-color: rgb(84, 122, 139);">Meetings</div>
-                    <div class="fc-event" data-color="#3EACAB" style="background-color: rgb(62, 172, 171); border-color: rgb(62, 172, 171);">Goals</div>
+                    <div class="fc-event" data-color="#2D95BF"
+                        style="background-color: rgb(45, 149, 191); border-color: rgb(45, 149, 191);">Holidays</div>
+                    <div class="fc-event" data-color="#48CFAE"
+                        style="background-color: rgb(72, 207, 174); border-color: rgb(72, 207, 174);">Leave Request
+                    </div>
+                    <div class="fc-event" data-color="#50C1E9"
+                        style="background-color: rgb(80, 193, 233); border-color: rgb(80, 193, 233);">Travel Request
+                    </div>
+                    <div class="fc-event" data-color="#FB6E52"
+                        style="background-color: rgb(251, 110, 82); border-color: rgb(251, 110, 82);">Upcoming Birthday
+                    </div>
+                    <div class="fc-event" data-color="#ED5564"
+                        style="background-color: rgb(237, 85, 100); border-color: rgb(237, 85, 100);">Trainings</div>
+                    <div class="fc-event" data-color="#355C7D"
+                        style="background-color: rgb(53, 92, 125); border-color: rgb(53, 92, 125);">Events</div>
+                    <div class="fc-event" data-color="#547A8B"
+                        style="background-color: rgb(84, 122, 139); border-color: rgb(84, 122, 139);">Meetings</div>
+                    <div class="fc-event" data-color="#3EACAB"
+                        style="background-color: rgb(62, 172, 171); border-color: rgb(62, 172, 171);">Goals</div>
                 </div>
             </div>
         </div>
@@ -23,31 +34,60 @@
         </div>
     </div>
 </div>
+<!-- Calendar Modal -->
+<div class="modal fade" id="createEventModal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Add Event</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" id="title" class="form-control" placeholder="Event Title">
+                </div>
+                <div class="form-group">
+                    <label for="start">Selected Date</label>
+                    <input type="text" name="start" id="start" class="form-control" readonly>
+                    <input type="hidden" name="end" id="end" class="form-control">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" name="submit" id="submit">Save</button>
+            </div>
+        </div>
+    </div>
+</div>
 @stop
 
 @push('stylesheets')
 <link rel="stylesheet" href="{{ asset('plugin/fullcalendar/main.min.css') }}" />
 <style>
-    #external-events{
+    #external-events {
         width: 240px;
         padding: 0 10px;
         border: 1px solid #ccc;
         background: #eee;
         text-align: left;
     }
-    
+
     #external-events .fc-event {
         margin: 10px 0;
         padding: 2px !important;
         cursor: pointer;
     }
+
 </style>
 @endpush
 
 @push('scripts')
-<script src="{{ asset('plugin/fullcalendar/main.min.js') }}"></script>    
+<script src="{{ asset('plugin/fullcalendar/main.min.js') }}"></script>
 <script>
-    $(document).ready(function (){
+    $(document).ready(function () {
         var calendar = $('#calendar').fullCalendar({
             editable: true,
             header: {
@@ -60,44 +100,20 @@
             editable: true,
             eventRender: function (event, element, view) {
                 if (event.allDay === 'true') {
-                        event.allDay = true;
+                    event.allDay = true;
                 } else {
-                        event.allDay = false;
+                    event.allDay = false;
                 }
             },
             selectable: true,
             selectHelper: true,
+            //When u select some space in the calendar do the following:
             select: function (start, end, allDay) {
-                var title = prompt('Event Title:');
-                if (title) {
-                    var start = $.fullCalendar.formatDate(start, "Y-MM-DD");
-                    var end = $.fullCalendar.formatDate(end, "Y-MM-DD");
-                    $.ajax({
-                        url: '{{ route("hr_calendar.ajax") }}',
-                        data: {
-                            _token: "{{ csrf_token() }}",
-                            title: title,
-                            start: start,
-                            end: end,
-                            type: 'add'
-                        },
-                        type: "POST",
-                        success: function (data) {
-                            displayMessage("Event Created Successfully");
-                            calendar.fullCalendar('renderEvent',
-                                {
-                                    id: data.id,
-                                    title: title,
-                                    start: start,
-                                    end: end,
-                                    allDay: allDay
-                                },true);
-
-                            calendar.fullCalendar('unselect');
-                        }
-                    });
-                }
+                $('#start').val($.fullCalendar.formatDate(start, "Y-MM-DD"));
+                $('#end').val($.fullCalendar.formatDate(end, "Y-MM-DD"));
+                $('#createEventModal').modal('show');
             },
+            //When u drop an event in the calendar do the following:
             eventDrop: function (event, delta) {
                 var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD");
                 var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD");
@@ -118,6 +134,7 @@
                     }
                 });
             },
+            //When u click an event in the calendar do the following:
             eventClick: function (event) {
                 var deleteMsg = confirm("Do you really want to delete?");
                 if (deleteMsg) {
@@ -125,9 +142,9 @@
                         type: "POST",
                         url: '{{ route("hr_calendar.ajax") }}',
                         data: {
-                                _token: "{{ csrf_token() }}",
-                                id: event.id,
-                                type: 'delete'
+                            _token: "{{ csrf_token() }}",
+                            id: event.id,
+                            type: 'delete'
                         },
                         success: function (response) {
                             calendar.fullCalendar('removeEvents', event.id);
@@ -136,11 +153,47 @@
                     });
                 }
             }
-        })
+        });
+
+        $('#submit').on('click', function(e){
+            //Initialize variables
+            var start = $('#start').val();
+            var end = $('#end').val();
+            var title = $('#title').val();
+            //We don't want this to act as a link so cancel the link action
+            e.preventDefault();
+            //Hide the modal
+            $("#createEventModal").modal('hide');
+            //Submit via ajax
+            $.ajax({
+                url: '{{ route("hr_calendar.ajax") }}',
+                data: {
+                    _token: "{{ csrf_token() }}",
+                    title: title,
+                    start: start,
+                    end: end,
+                    type: 'add'
+                },
+                type: "POST",
+                success: function (data) {
+                    displayMessage("Event Created Successfully");
+                    calendar.fullCalendar('renderEvent',
+                        {
+                            id: data.id,
+                            title: title,
+                            start: start,
+                            end: end
+                        },true);
+                    calendar.fullCalendar('unselect');
+                }
+            });
+        });
+
+
+        function displayMessage(message) {
+            alert(message);
+        } 
     });
 
-    function displayMessage(message) {
-        alert(message);
-    } 
 </script>
 @endpush
