d97d1cb90637781339b401d45bb61118bc31d6cf
Paul Jao <paul.jao95@gmail.com>
2021-02-23 15:01:15 +0800

    wip main kpi datatable

---
+++ app/DataTables/KpiMaingoalDataTable.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\KpiMaingoal;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class KpiMaingoalDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('action', 'kpimaingoal.action');
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\Models\KpiMaingoal $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(KpiMaingoal $model)
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+            ->setTableId('kpimain-table')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            ->parameters([
+                'order' => [
+                    0,
+                    'desc'
+                ],
+                'buttons' => [
+                    'create'
+                ],
+                'language' => [
+                    'search' => '',
+                    'searchPlaceholder' => __('label.global.datatable.text.search_placeholder'),
+                    'loadingRecords' => '&nbsp;',
+                    'processing' => '<div class="text-center"><div class="spinner-border" role="status">
+                        <span class="sr-only">' . __('label.global.datatable.text.loading') . '</span></div></div>'
+                ],
+                'drawCallback' => "function (settings, json) {" .
+                    "$('.copy-btn').tooltip();}"
+            ])->dom("<'row mb-2'<'col-sm-12 col-md-6'B><'col-sm-12 col-md-6'f>>" .
+                "<'row'<'col-sm-12 col-md-12't><'col-sm-12 col-md-12'r>>" .
+                "<'row'<'col-sm-12 col-md-6'p>>"
+            )->orderBy(1);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            Column::computed('action')
+                ->title(__('label.kpi_main.datatable.column_header.action')),
+            Column::make('main_kpi')
+                ->title(__('label.kpi_main.datatable.column_header.main_kpi')),
+            Column::make('q1')
+                ->title(__('label.kpi_main.datatable.column_header.q1')),
+            Column::make('q2')
+                ->title(__('label.kpi_main.datatable.column_header.q2')),
+            Column::make('q3')
+                ->title(__('label.kpi_main.datatable.column_header.q3')),
+            Column::make('q4')
+                ->title(__('label.kpi_main.datatable.column_header.q4')),
+            Column::make('status')
+                ->title(__('label.kpi_main.datatable.column_header.completed')),
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        // return 'KpiMaingoal_' . date('YmdHis');
+    }
+}
