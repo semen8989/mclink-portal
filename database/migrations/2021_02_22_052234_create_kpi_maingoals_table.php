24e491ecdac4f80f37354d4dce718bde3de1c15c
Paul Jao <paul.jao95@gmail.com>
2021-02-23 08:20:15 +0800

    added migration files for kpi maingoals and kpi ratings table

---
+++ database/migrations/2021_02_22_052234_create_kpi_maingoals_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKpiMaingoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kpi_maingoals', function (Blueprint $table) {
+            $table->id();
+            $table->mediumText('main_kpi');
+            $table->mediumText('q1')->nullable();
+            $table->mediumText('q2')->nullable();
+            $table->mediumText('q3')->nullable();
+            $table->mediumText('q4')->nullable();
+            $table->mediumText('feedback')->nullable();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('kpi_rating_id')->references('id')->on('kpi_ratings');
+            $table->tinyInteger('status');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kpi_maingoals');
+    }
+}
