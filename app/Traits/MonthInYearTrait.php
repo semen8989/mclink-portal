b06e72db6dae05a43645d040d9a462a2d20c6de8
Paul Jao <paul.jao95@gmail.com>
2021-02-23 14:59:13 +0800

    added monthinyeartrait for a reusuable access to months in a year

---
+++ app/Traits/MonthInYearTrait.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Traits;
+
+trait MonthInYearTrait {
+    public static $month = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+
+    public function get ()
+    {
+
+    }
+}
