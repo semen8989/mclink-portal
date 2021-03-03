834195870691ffb8d12383f925b1950534773fa7
Kassandra Lim <sandralim298@gmail.com>
2021-03-03 14:08:23 +0800

    Insert event and rerender events on calendar after insert

--- app/Http/Controllers/HrCalendarController.php
+++ app/Http/Controllers/HrCalendarController.php
@@ -23,7 +23,46 @@ class HrCalendarController extends Controller
                 'color' => $row->color,
             ];
         }
-        return view('hr_calendar.index',['events' => json_encode($data)]);
+        $companies = Company::all('id','company_name');
+        return view('hr_calendar.index',compact('companies'));
+    }
+
+    public function fetch_events(){
+        $data = [];
+        $result = Event::all();
+        foreach($result as $row) {
+            $data[] = [
+                'id' => $row->id,
+                'title' => $row->title,
+                'start' => $row->start_date,
+                'end' => $row->end_date,
+                'color' => $row->color,
+            ];
+        }
+
+        echo json_encode($data);
     }
     
+    public function store_event(Request $request)
+    {
+        $insert = [
+            'company_id' => $request->company_id,
+            'title'      => $request->title,
+            'color'      => '#355C7D',
+            'start_date' => date('Y-m-d', strtotime($request->start_date)),
+            'end_date'   => date('Y-m-d', strtotime($request->end_date)),
+            'note'       => $request->note
+        ];
+        $create = Event::create($insert);
+        if($create){
+            $data['success'] = true;
+            $data['message'] = "Event created successfully";
+        }else{
+            $data['success'] = false;
+            $data['message'] = "Error while creating successfully";
+        }
+
+        echo json_encode($data);
+    }
+
 }
--- resources/views/hr_calendar/index.blade.php
+++ resources/views/hr_calendar/index.blade.php
@@ -7,6 +7,56 @@
         <div id="calendar-1"></div>
     </div>
 </div>
+<!-- Create event modal -->
+<div class="modal fade" id="createEvent_modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <form method="POST" id="createEvent_form" action="{{ route('hr_calendar.store_event') }}">
+            @csrf
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="company_id" class="col-form-label">Company ID</label>
+                    <select class="form-control" name="company_id" id="company_id">
+                        @foreach($companies as $company)
+                            <option value="{{ $company->id }}">{{ $company->company_name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="title" class="col-form-label">Title</label>
+                    <input type="text" class="form-control" name="title" id="title"></input>
+                </div>
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="start_date">Start Date</label>
+                        <input type="text" class="form-control" name="start_date" id="start_date">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="end_date">End Date</label>
+                        <input type="text" class="form-control" name="end_date" id="end_date">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="form-group">
+                        <label for="note">Event Note</label>
+                        <textarea class="form-control" name="note" id="note" cols="30" rows="10"></textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+        </form>
+      </div>
+    </div>
+  </div>
 @stop
 
 @push('stylesheet')
@@ -36,16 +86,54 @@
                     headerToolbar:{
                         end:"today prev,next dayGridMonth timeGridWeek timeGridDay"
                     },
-                    events: {!! $events !!},
+                    events: "{{ route('hr_calendar.fetch_events') }}",
                     select:function(selectionInfo){
-                        alert(FullCalendar.formatDate(selectionInfo.start))
+                        $('#createEvent_modal').modal('show');
+                        $('#start_date').val(FullCalendar.formatDate(selectionInfo.start));
+                        $('#end_date').val(FullCalendar.formatDate(selectionInfo.end));
                     },
                     eventClick:function(event){
-                        alert(event.event.title)
+                        alert(event.event.title);
                     }
                 },
             );
             calendar.render();
+
+            $('#createEvent_form').submit(function (e){
+                e.preventDefault();
+
+                var url = $(this).attr('action');
+                var method = $(this).attr('method');
+                var data = $(this).serialize();
+
+                $.ajax({
+                    url: url,
+                    data: data,
+                    method: method,
+                    dataType: 'json',
+                    encode: true,
+                    success: function(data){
+                        if(data.success == true){
+                            //remove any form data
+                            $('#createEvent_form').trigger("reset");
+                            //close model
+                            $('#createEvent_modal').modal('hide');
+                            //Alert
+                            alert(data.message);
+                            //refetch events
+                            calendar.refetchEvents();
+                        }else if(data.success == false){
+                            //Alert
+                            alert(data.message);
+                        }
+                    }
+                })
+            });
         });
+         // so your code will be
+         $('#createEvent_modal').on('hidden.bs.modal', function () {
+            
+        });
+
     </script>
 @endpush
