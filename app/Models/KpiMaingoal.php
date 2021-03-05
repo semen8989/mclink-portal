5d04cf3ce90278ad69a5ef921b810a56b389ced6
Paul Jao <paul.jao95@gmail.com>
2021-03-05 17:29:22 +0800

    updated Kpimangoal and kpirating model for the update route

--- app/Models/KpiMaingoal.php
+++ app/Models/KpiMaingoal.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use App\Models\KpiRating;
+use App\Traits\MonthInYearTrait;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class KpiMaingoal extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, MonthInYearTrait;
 
     const COMPLETED_STATUS = [
         "no" => 0,
@@ -30,11 +31,29 @@ class KpiMaingoal extends Model
         'status' => 0,
     ];
 
+    public function getCompletedStatus()
+    {
+        return self::COMPLETED_STATUS;
+    }
+
+    public function getMonthList()
+    {
+        return self::$monthList;
+    }
+
     /**
      * Get the user that is associated with the record.
      */
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    /**
+     * Get all of the KpiMaingoal's kpi rating.
+     */
+    public function kpiratings()
+    {
+        return $this->morphMany(KpiRating::class, 'kpi_ratable');
     }
 }
--- app/Models/KpiRating.php
+++ app/Models/KpiRating.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class KpiRating extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the parent kpi_ratable model (maingoal, variable or objective).
+     */
+    public function kpi_ratable()
+    {
+        return $this->morphTo();
+    }
 }
