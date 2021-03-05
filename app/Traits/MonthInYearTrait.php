bb0a01fad83d884a4e753766986c941c289d0fec
Paul Jao <paul.jao95@gmail.com>
2021-03-05 17:28:30 +0800

    updated the monthinyear trait to be used on okr module

--- app/Traits/MonthInYearTrait.php
+++ app/Traits/MonthInYearTrait.php
@@ -3,23 +3,29 @@
 namespace App\Traits;
 
 trait MonthInYearTrait {
-    public static $month = [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
+
+    public static $monthList = [        
+        1 => 'January',
+        2 => 'February',
+        3 => 'March',
+        4 => 'April',
+        5 => 'May',
+        6 => 'June',
+        7 => 'July',
+        8 => 'August',
+        9 => 'September',
+        10 => 'October',
+        11 => 'November',
+        12 => 'December'
     ];
 
-    public function get ()
+    public function getStringMonth($month)
     {
+        return $this->monthList[$month];
+    }
 
+    public function getNumericMonth($month)
+    {
+        return array_search($month, $this->monthList);
     }
 }
