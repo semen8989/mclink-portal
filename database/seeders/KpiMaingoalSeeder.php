e370b31c07148f4a909ff300fde0ae22a3da3f9c
Paul Jao <paul.jao95@gmail.com>
2021-03-09 09:09:25 +0800

    updated the seeder for kpi main goal to properly show the datatable

--- database/seeders/KpiMaingoalSeeder.php
+++ database/seeders/KpiMaingoalSeeder.php
@@ -19,11 +19,11 @@ class KpiMaingoalSeeder extends Seeder
     public function run()
     {
         DB::table('kpi_maingoals')->insert([
-            'main_kpi' => Str::random(30),
-            'q1' => Str::random(20),
-            'q2' => Str::random(20),
-            'q3' => Str::random(20),
-            'q4' => Str::random(20),
+            'main_kpi' => Str::random(20),
+            'q1' => Str::random(10),
+            'q2' => Str::random(10),
+            'q3' => Str::random(10),
+            'q4' => Str::random(10),
             'user_id' => User::find(1)->id,
             'status' => 0,
             'created_at' =>  Carbon::now()->format('Y-m-d H:i:s'),
