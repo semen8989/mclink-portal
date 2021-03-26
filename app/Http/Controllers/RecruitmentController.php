df85116e6b57aff60bd6a74664e27ce5e000f2be
Kassandra Lim <sandralim298@gmail.com>
2021-03-26 16:19:40 +0800

    Display data on table in index file

--- app/Http/Controllers/RecruitmentController.php
+++ app/Http/Controllers/RecruitmentController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class RecruitmentController extends Controller
 {
     public function index()
     {
-        return view('recruitment.index');
+        $collection = Http::get('https://api.jotform.com/form/'.env('APPLICATION_FORM_ID').'/submissions?apiKey='.env('APPLICATION_FORM_API').'&limit=10');
+        return view('recruitment.index',['collection'=>$collection['content']]);
     }
 }
--- resources/views/recruitment/index.blade.php
+++ resources/views/recruitment/index.blade.php
@@ -1,9 +1,41 @@
 @extends('layout.master')
 
 @section('content')
-<div class="card-header">Recruitment List</div>
-<div class="card-body">
-    
-</div>
+    <div class="card-header">Recruitment List</div>
+    <div class="card-body">
+        <table class="table table-responsive-sm">
+            <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Position Applying For</th>
+                <th>Gender</th>
+            </tr>
+            <?php $i = 1; ?>
+            @foreach($collection as $item)
+                <tr>
+                    <td>{{ $i++ }}</td>
+                    <td>{{ $item['answers']['15']['answer']['first'].' '.$item['answers']['15']['answer']['last'] }}</td>
+                    <td>{{ $item['answers']['11']['answer'] }}</td>
+                    <td>{{ $item['answers']['16']['answer'] }}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 @stop
+
+@push('stylesheet')
+    <!-- jquery datatable button and responsive extension css dependency -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.0.3/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.7/css/responsive.dataTables.min.css">
+@endpush
+
+@push('scripts')
+    <!-- jquery datatable button and responsive extension js dependency -->
+    <script src="https://cdn.datatables.net/buttons/1.0.3/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.7/js/dataTables.responsive.min.js"></script>
+
+    <!-- laravel datatable button plugin js dependency -->
+    <script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
+    
+@endpush
 
