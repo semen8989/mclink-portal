13247865c54b4aa090dee569e2ae9b36cd935630
Paul Jao <paul.jao95@gmail.com>
2021-05-06 17:04:32 +0800

    updated user seeder to generate 2 default admin user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,13 +2,10 @@
 
 namespace Database\Seeders;
 
-use Carbon\Carbon;
 use App\Models\Role;
 use App\Models\User;
 use App\Models\Ability;
-use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
@@ -20,24 +17,32 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //Create admin user
-        $admin = User::create([
-            'name' => 'Root',
-            'email' => 'root@mclinkgroup.com',
-            'email_verified_at' => now(),
-            'password' => Hash::make('password'),
-            'employee_id' => '0101',
-            'joining_date' => '2021-01-18',
-            'company_id' => 1,
-            'department_id' => 2,
-            'designation_id' => 1,
-            'gender' => 'female',
-            'shift_id' => 1,
-            'birth_date' => '1998-04-21',
-            'contact_number' => '09274302437'
+        $users = [];
 
-        ]);
-        //Assign role
-        $admin->assignRole('Administrator');
+        for ($counter = 0; $counter < 2; $counter++) { 
+            array_push($users, [
+                'name' => 'Root' . strval($counter + 1),
+                'email' => 'root' . strval($counter + 1) . '@mclinkgroup.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'employee_id' => '0101',
+                'joining_date' => '2021-01-18',
+                'company_id' => 1,
+                'department_id' => 2,
+                'designation_id' => 1,
+                'gender' => 'female',
+                'shift_id' => 1,
+                'birth_date' => '1998-04-21',
+                'contact_number' => '09274302437'
+            ]);
+        }
+
+        foreach ($users as $user) {
+            //Create admin user
+            $admin = User::create($user);
+
+            //Assign role to the admin user
+            $admin->assignRole('Administrator');
+        }     
     }
 }
