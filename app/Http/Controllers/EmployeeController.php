8bdfe96935adeddf92d90443186ebc32ad4e124c
Paul Jao <paul.jao95@gmail.com>
2021-10-26 16:56:00 +0800

    removed current user from the list of fetched employees

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class EmployeeController extends Controller
 {
@@ -13,10 +14,11 @@ class EmployeeController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function get(Request $request)
-    {            
-        $customers = User::select('id','name', 'designation_id')
-            ->with('designation:id,designation_name')         
+    {
+        $customers = User::select('id', 'name', 'designation_id')
+            ->with('designation:id,designation_name')
             ->where([['name', 'like', '%'.$request->query('search').'%']])
+            ->where('id', '!=', Auth::id())
             ->orderBy('name', 'asc')
             ->paginate(10);
 
