8bc6ae58c2ffa3459dc497643b59e337b76df544
Kassandra Lim <sandralim298@gmail.com>
2022-01-04 15:56:26 +0800

    Disable submit button after clicked

--- resources/views/sales_lead/create.blade.php
+++ resources/views/sales_lead/create.blade.php
@@ -173,6 +173,8 @@
                     beforeSend: function() { 
                         $(".help-block").remove();
                         $( ".form-control" ).removeClass("is-invalid");
+                        $(".btn-submit").attr("disabled", true);
+                        $(".btn-submit").html(`<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Sending...`);
                     },
                     success: function(data){
                         if(data.success == false)
@@ -209,6 +211,10 @@
                             },500);
                         }
                         
+                    },
+                    complete: function() {
+                        $(".btn-submit").attr("disabled", false);
+                        $(".btn-submit").html('Submit');
                     }
                 })
             })
